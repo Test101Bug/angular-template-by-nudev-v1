f888f05564cc83a4eae3aebffc1cb8ce8852acdf
Test101Bug <zookeroro@gmail.com>
2020-08-18 14:24:22 +0700

    add bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './layouts/main/main.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'main', component: MainComponent, children: [] },
 ];
 
 @NgModule({
